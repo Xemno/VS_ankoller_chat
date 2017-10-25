cfbef51db5d7fff6207b72ebe24d55377ec57fd6
anjakoller <32456749+anjakoller@users.noreply.github.com>
2017-10-25 15:49:03 +0200

    Anja commit test

--- app/src/main/java/ch/ethz/inf/vs/a3/ankoller/chat/MainActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a3/ankoller/chat/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Initial files
         //first change
+        //change 2
     }
 }
