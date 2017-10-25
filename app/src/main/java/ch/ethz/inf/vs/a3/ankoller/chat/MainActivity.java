f8fc8d1e40429ef9277da4e0c7d5a2712c39350f
anjakoller <32456749+anjakoller@users.noreply.github.com>
2017-10-25 16:49:24 +0200

    Anja Commit 25-10 buttons join und setting kreiert inkl edittext (in layout activity main) - (Task 1.1 und 1.2 done) und onCLickmethoden für beide buttons in main erstellt und zugehörige Activities, werde ich morgen oder heute abend noch weiterimplementieren

--- app/src/main/java/ch/ethz/inf/vs/a3/ankoller/chat/MainActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a3/ankoller/chat/MainActivity.java
@@ -1,7 +1,9 @@
 package ch.ethz.inf.vs.a3.ankoller.chat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
         // Initial files
         //first change
         //change 2
+    }
+    public void OnClickJoin(View view){
+        //method for what happens when joinbutton is clicked
+        //also declared in the layout file android:OnClick in the join button
+        //layout android_mainactivity
+        //sends a message to the erceiver
+        //on successful registration, application must transition
+        //to a new activity (Chatactivity)
+        //if registration not successful chat client tries automatically again
+        //to register to the user 5 times, if still not successful,
+        //application must display a notification like "registration not possible"
+
+
+    }
+    public void OnSettingsClick (View view){
+        //method for what happens when settingbutton is clicked, also ins the layout file
+        //int he settings button
+        //transitions to another activity (Settingsactivity)
+        //user should be able to set servers address and port --> see SettingsActivity class
+        startActivity(new Intent(this,SettingsActivity.class));
+
     }
 }
