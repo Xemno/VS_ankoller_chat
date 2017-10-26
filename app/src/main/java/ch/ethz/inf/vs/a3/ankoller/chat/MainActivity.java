261e60270c16e66d341f4b0f1a5ac66fde357446
anjakoller <32456749+anjakoller@users.noreply.github.com>
2017-10-26 22:06:12 +0200

    anja commit 26_10 Settingsactivity implementier, vermutlich fehlt noch etwas, geht noch nicht, wenn man auf settingsbutton klickt stürzt es ab, weiss jemand woran das liegt?

--- app/src/main/java/ch/ethz/inf/vs/a3/ankoller/chat/MainActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a3/ankoller/chat/MainActivity.java
@@ -1,9 +1,13 @@
 package ch.ethz.inf.vs.a3.ankoller.chat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +29,15 @@ public class MainActivity extends AppCompatActivity {
         //if registration not successful chat client tries automatically again
         //to register to the user 5 times, if still not successful,
         //application must display a notification like "registration not possible"
-
+       //check if there is internet connection
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo == null || !networkInfo.isConnected()){
+            Toast.makeText(getApplication(), "no internetconnection!", Toast.LENGTH_LONG).show();
+        }else{
+            //TODO
+            //declare, username, userid,... to at last call a thread wich tries again registration
+        }
 
     }
     public void OnSettingsClick (View view){
--- app/src/main/java/ch/ethz/inf/vs/a3/ankoller/chat/SettingsActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a3/ankoller/chat/SettingsActivity.java
@@ -1,8 +1,37 @@
 package ch.ethz.inf.vs.a3.ankoller.chat;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import ch.ethz.inf.vs.a3.ankoller.chat.udpclient.NetworkConsts;
+
 /**
  * Created by anja on 25.10.2017.
  */
 
-public class SettingsActivity {
+public class SettingsActivity extends FragmentActivity {
+    //user should be able to set server adress and port --> modify xml file (preferences.xml)
+    //declare here
+    EditText editadress;
+    EditText editport;
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.preferences);
+
+        editadress = (EditText)findViewById(R.id.edit_adress);
+        editport = (EditText)findViewById(R.id.edit_port);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        editadress.setText(sharedPreferences.getString("key_stored_string_textview_adress", NetworkConsts.SERVER_ADDRESS));
+        editport.setText(String.valueOf(sharedPreferences.getInt("key_stored_int_textview_port", NetworkConsts.UDP_PORT)));
+    }
+
+
+
+
 }
